9a6a9a4e72e53ece42f420bb5537c72b64b63157
Eugene Marikhin <emarikhin@gmail.com>
2020-09-02 11:43:18 +1200

    changes in Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import s from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
+import stylename from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-    <div>
-        Dialogs
-    </div>
+        <div className={stylename.dialogs}>
+            <div className={stylename.dialogsItems}>
+                <div className={stylename.dialog + ' ' + stylename.active}>
+                    <NavLink to='/dialogs/1' activeClassName={stylename.activeLink}>Eugene</NavLink>
+                </div>
+                <div className={stylename.dialog}>
+                    <NavLink to='/dialogs/2' activeClassName={stylename.activeLink}>Irison</NavLink>
+                </div>
+                <div className={stylename.dialog}>
+                    <NavLink to='/dialogs/3' activeClassName={stylename.activeLink}>Ronny</NavLink>
+                </div>
+            </div>
+            <div className={stylename.messages}>
+                <div className={stylename.message}>Hey peeps!</div>
+                <div className={stylename.message}>Sweetaz</div>
+                <div className={stylename.message}>Chur</div>
+            </div>
+        </div>
     );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import cl from './Navbar.module.css';
+import customstyle from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-    return <nav className={cl.nav}>
-        <div className={cl.item}>
-            <a href='/profile'>Profile</a>
+    return (
+    <nav className={customstyle.nav}>
+        <div className={customstyle.item}>
+            <NavLink to='/profile' activeClassName={customstyle.activeLink}>Profile</NavLink>
         </div>
-        <div className={`${cl.item} ${cl.active}`}>
-            <a href='/dialogs'>Messages</a>
+        <div className={`${customstyle.item} ${customstyle.active}`}>
+            <NavLink to='/dialogs' activeClassName={customstyle.activeLink}>Messages</NavLink>
         </div>
-        <div className={cl.item}>
-            <a href='/news'>News</a>
+        <div className={customstyle.item}>
+            <NavLink to='/news' activeClassName={customstyle.activeLink}>News</NavLink>
         </div>
-        <div className={cl.item}>
-            <a href='/music'>Music</a>
+        <div className={customstyle.item}>
+            <NavLink to='/music' activeClassName={customstyle.activeLink}>Music</NavLink>
         </div>
-        <div className={cl.item}>
-            <a href='/settings'>Settings</a>
+        <div className={customstyle.item}>
+            <NavLink to='/settings' activeClassName={customstyle.activeLink}>Settings</NavLink>
         </div>
     </nav>
+    )
 }
 
 export default Navbar; 
